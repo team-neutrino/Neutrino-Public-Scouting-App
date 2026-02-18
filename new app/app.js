dfd70116b881ceaee69c2aa83a67ab2fd38c6a75
jackson-diekmann <jacksondiekmann365@gmail.com>
2026-02-18 17:45:35 -0600

    initial

---
+++ new app/app.js
@@ -0,0 +1,15 @@
+let array = [
+  "score fuel",
+  "action1"
+]
+
+function addAction(numbe) {
+  compressList.push(number);
+  
+  let string = "";
+  for (let action in compressList) {
+    string += array[action] + "\n"
+  }
+
+  document.getElementById('terminal').innerHTML = string;
+}
